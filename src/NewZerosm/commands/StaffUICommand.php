18bed4dbb9f3aeb88a62d6c4ba4b75dba5e884ea
MineDefenser <mcpesunho@naver.com>
2018-08-20 19:02:41 +0900

    Bugs Fix Version

--- src/NewZerosm/commands/StaffUICommand.php
+++ src/NewZerosm/commands/StaffUICommand.php
@@ -10,13 +10,8 @@ use pocketmine\command\CommandExecutor;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\item\Item;
 
-use pocketmine\network\mcpe\protocol\types\CommandEnum;
-use pocketmine\network\mcpe\protocol\types\CommandParameter;
-
 use NewZerosm\StaffMenu;
 use NewZerosm\ZerosmCommand;
-
-use onebone\economyapi\EconomyAPI;
 
 class StaffUICommand extends ZerosmCommand{
 	
@@ -52,15 +47,10 @@ class StaffUICommand extends ZerosmCommand{
 				
 				switch($result){
 					case 0:
-						$command = "3dtouch";
-						$sender->getServer()->getCommandMap()->dispatch($sender, $command);
-					break;
-					
-					case 1:
 						$sender->sendMessage("§c아직 준비중인 기능입니다. 빠른 시일내로 준비하겠습니다.");
 					break;
 					
-					case 2:
+					case 1:
 						$api = $sender->getServer()->getPluginManager()->getPlugin("FormAPI");
 						if ($api === null || $api->isDisabled()) {
 						}
@@ -94,11 +84,11 @@ class StaffUICommand extends ZerosmCommand{
 						$form->sendToPlayer($sender);
 					break;	
 					
-					case 3:
+					case 2:
 						$sender->sendMessage("§c아직 준비중인 기능입니다. 빠른 시일내로 준비하겠습니다.");
 					break;
 					
-					case 4:
+					case 3:
 						$api = $sender->getServer()->getPluginManager()->getPlugin("FormAPI");
 						if ($api === null || $api->isDisabled()) {
 						}
@@ -117,7 +107,7 @@ class StaffUICommand extends ZerosmCommand{
 								$sender->sendMessage("§f<§e차단§f> 사유: §e".$reason."");
 								$sender->sendMessage("§7----------------");
 								
-								$player->kick("서버에서 Kick 처리 되었습니다.\n§f사유: §e".$reason."", false);
+								$player->kick("서버에서 Kick 처리 되었습니다.\n§f사유: §e".$reason."\n§f몇초뒤에 서버에 재접속 해보세요!", false);
 								return true;
 							}
 					
@@ -133,7 +123,7 @@ class StaffUICommand extends ZerosmCommand{
 						$form->sendToPlayer($sender);
 					break;
 					
-					case 5:
+					case 4:
 						$api = $sender->getServer()->getPluginManager()->getPlugin("FormAPI");
 						if ($api === null || $api->isDisabled()) {
 						}
@@ -172,7 +162,7 @@ class StaffUICommand extends ZerosmCommand{
 						$form->sendToPlayer($sender);
 					break;
 								
-					case 6:
+					case 5:
 						$api = $sender->getServer()->getPluginManager()->getPlugin("FormAPI");
 						if ($api === null || $api->isDisabled()) {
 						}
@@ -202,7 +192,7 @@ class StaffUICommand extends ZerosmCommand{
 						$form->sendToPlayer($sender);
 					break;
 				  
-					case 7:
+					case 6:
 						$api = $sender->getServer()->getPluginManager()->getPlugin("FormAPI");
 						if ($api === null || $api->isDisabled()) {
 						}
@@ -244,7 +234,7 @@ class StaffUICommand extends ZerosmCommand{
 						$form->sendToPlayer($sender);
 					break;
 								
-					case 8:
+					case 7:
 						$api = $sender->getServer()->getPluginManager()->getPlugin("FormAPI");
 						if ($api === null || $api->isDisabled()) {
 						}
@@ -280,15 +270,14 @@ class StaffUICommand extends ZerosmCommand{
 			});
 			$form->setTitle("§l§7<< §bStaff Menu §7>>");
 			$form->setContent("차단 메뉴에 오신것을 환영합니다! 관리자님!");
-			$form->addButton("§l§9<< §c메인으로 돌아가기 §9>>\n§r§fAssistiveTouch 메인화면으로 이동합니다.", 0);
-			$form->addButton("§l§8<< §aServer Message §8>>\n§r§f공지 보내기", 1, "https://i.imgur.com/3llO5u6.png");
-			$form->addButton("§l§8<< §aUser Date Info §8>>\n§r§f유저 정보 조회", 2, "https://i.imgur.com/3llO5u6.png");
-			$form->addButton("§l§8<< §aUser Inventory Clear §8>>\n§r§f인벤토리 초기화", 3, "https://i.imgur.com/3llO5u6.png");
-			$form->addButton("§l§8<< §aKick §8>>\n§r§f플레이어 킥", 4, "https://i.imgur.com/3llO5u6.png");
-			$form->addButton("§l§8<< §aName Ban §8>>\n§r§f닉네임 차단", 5, "https://i.imgur.com/3llO5u6.png");
-			$form->addButton("§l§8<< §aName UnBan §8>>\n§r§f닉네임 차단 해제", 6, "https://i.imgur.com/3llO5u6.png");
-			$form->addButton("§l§8<< §aIP Ban §8>>\n§r§f아이피 차단", 7, "https://i.imgur.com/3llO5u6.png");
-			$form->addButton("§l§8<< §aIP UnBan §8>>\n§r§f아이피 차단 해제", 8, "https://i.imgur.com/3llO5u6.png");
+			$form->addButton("§l§8<< §aServer Message §8>>\n§r§f공지 보내기");
+			$form->addButton("§l§8<< §aUser Date Info §8>>\n§r§f유저 정보 조회");
+			$form->addButton("§l§8<< §aUser Inventory Clear §8>>\n§r§f인벤토리 초기화");
+			$form->addButton("§l§8<< §aKick §8>>\n§r§f플레이어 킥");
+			$form->addButton("§l§8<< §aName Ban §8>>\n§r§f닉네임 차단");
+			$form->addButton("§l§8<< §aName UnBan §8>>\n§r§f닉네임 차단 해제");
+			$form->addButton("§l§8<< §aIP Ban §8>>\n§r§f아이피 차단");
+			$form->addButton("§l§8<< §aIP UnBan §8>>\n§r§f아이피 차단 해제");
 			$form->sendToPlayer($sender);
 			return true;
 			
